102ae8999e6ccb4ff84a9640f0f97d2985cf2836
eprahemapdala10-web <eprahemapdala10@gmail.com>
2026-06-25 15:13:03 +0300

    Add component exports

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export { default as MaintenanceWizard } from './MaintenanceWizard';
+export { default as PartsStore } from './PartsStore';
